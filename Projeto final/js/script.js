f439ba7214c905bda60787d2bdd8e2bbe1eb162e
Rafaela <lrafaela1632@gmail.com>
2025-12-10 15:00:12 -0300

    fix: carrossel

--- Projeto final/js/script.js
+++ Projeto final/js/script.js
@@ -1,15 +1,42 @@
 // função de salvar o pet
-const icon = document.getElementById("favoriteIcon");
+const icons = document.querySelectorAll(".heart-icon");
 
-icon.addEventListener("click", function () {
-  if (this.classList.contains("favorito")) {
-    this.classList.remove("favorito");
-    alert("Pet removido dos favoritos!");
-  } 
-  else {
-    this.classList.add("favorito");
-    alert("Pet salvo!");
-  }
+icons.forEach(icon => {
+  icon.addEventListener("click", function () {
+    if (this.classList.contains("favorito")) {
+      this.classList.remove("favorito");
+      alert("Pet removido dos favoritos!");
+    } 
+    else {
+      this.classList.add("favorito");
+      alert("Pet salvo!");
+    }
+  });
 });
 
+// função de carrossel
+const container = document.querySelector(".cards-container");
+const arrowLeft = document.getElementById("arrowLeft");
+const arrowRight = document.getElementById("arrowRight");
 
+let index = 0;
+const cardWidth = 320 + 32; 
+const maxCards = document.querySelectorAll(".card").length;
+const cardsVisible = 2;
+const jump = 2; 
+
+arrowLeft.addEventListener("click", () => {
+    if (index < maxCards - cardsVisible) {
+        index += jump;
+        if (index > maxCards - cardsVisible) index = maxCards - cardsVisible;
+        container.style.transform = `translateX(-${index * cardWidth}px)`;
+    }
+});
+
+arrowRight.addEventListener("click", () => {
+    if (index > 0) {
+        index -= jump;
+        if (index < 0) index = 0;
+        container.style.transform = `translateX(-${index * cardWidth}px)`;
+    }
+});
